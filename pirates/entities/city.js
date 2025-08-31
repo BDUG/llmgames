5385036cf883c24cce4f8245e27b2bc2533eff4b
BDUG <dirk.bangel@gmx.de>
2025-08-31 12:35:47 +0200

    Use images for ship and city

--- pirates/entities/city.js
+++ pirates/entities/city.js
@@ -1,3 +1,5 @@
+import { assets } from '../assets.js';
+
 export class City {
   constructor(x, y, name) {
     this.x = x;
@@ -6,7 +8,12 @@ export class City {
   }
 
   draw(ctx) {
-    ctx.fillStyle = 'gray';
-    ctx.fillRect(this.x - 8, this.y - 8, 16, 16);
+    const img = assets.city;
+    if (img) {
+      ctx.drawImage(img, this.x - img.width / 2, this.y - img.height / 2);
+    } else {
+      ctx.fillStyle = 'gray';
+      ctx.fillRect(this.x - 8, this.y - 8, 16, 16);
+    }
   }
 }
